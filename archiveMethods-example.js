5c3cc44f134514738a32d6bb4cee6def81e64f0b
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2022-01-14 15:30:25 +0100

    added example

--- archiveMethods-example.js
+++ archiveMethods-example.js
@@ -29,23 +29,34 @@ const archiveMethods = {
   },
   VISVarsel: {
     id: 'VISVarsel',
-    name: '"Varsel om fare for manglende vurderingsgrunnlag i fag  - VO (voksenopplæring)',
+    name: 'Varsel om fare for manglende vurderingsgrunnlag i fag  - VO (voksenopplæring)',
     test: true,
     findDataMethod: 'visVarselDoc',
-    identifierStrings: ['Varsel om fare for manglende vurderingsgrunnlag i fag', 'Kompetansebyggeren'],
+    identifierStrings: ['Varsel om fare for manglende vurderingsgrunnlag i fag', 'Kompetansebyggeren', 'Varselet gjelder:'],
     archiveTemplate: 'varsel-vo',
     svarUt: false,
     manualSvarUt: false,
     schoolOrgnr: '994309153',
     accessGroup: 'Elev Kompetansebyggeren'
+  },
+  VISKarakterProt: {
+    id: 'VISKarakterProt',
+    name: 'VIS Karakterprotokoll',
+    test: true,
+    findDataMethod: 'vis-karakter-prot',
+    identifierStrings: ['Karakterutskrift', 'Navn:', 'Fødselsnummer:', 'Programområde', 'Karakter', 'Fravær hele dager', 'Fravær enkelttimer', 'Atferd'],
+    archiveTemplate: 'karakterprotokoll',
+    svarUt: false,
+    manualSvarUt: false
   }
 }
 
+const standardDocs = ['visVarselDoc', 'visKarakterProt']
 const soknader = []
 const visStandardDocs = []
 for (const val of Object.values(archiveMethods)) {
   if (val.findDataMethod === 'soknad') soknader.push(val)
-  else if (val.findDataMethod === 'visVarselDoc') visStandardDocs.push(val)
+  else if (standardDocs.includes(val.findDataMethod)) visStandardDocs.push(val)
 }
 
 module.exports = { archiveMethods, soknader, visStandardDocs }
