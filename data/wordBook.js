f35772e2abc4344db39c5d33fdd4a2ef23cf93df
jorgtho <jorgen.thorsnes@gmail.com>
2022-03-30 14:48:01 +0200

    wordbook to easier finding fields when recognizing text

---
+++ data/wordBook.js
@@ -0,0 +1,38 @@
+const wordBook = [
+  'Fødselsnummer:',
+  'Navn:',
+  'Elevens navn:',
+  'Elev:',
+  'Klasse:',
+  'Fødselsdato:',
+  'Dato:',
+  'Sted:',
+  'Skole:'
+  /*
+    'Bamble',
+    'Nøtterøy',
+    'Horten',
+    'Rjukan',
+    'Greveskogen',
+    'Skien',
+    'Kragerø',
+    'Notodden',
+    'Porsgrunn',
+    'Vest-Telemark',
+    'Hjalmar Johansen',
+    'Skogmo',
+    'Færder',
+    'Sande',
+    'Sandefjord',
+    'Kompetansebyggeren',
+    'Melsom',
+    'Skolen for sosiale og medisinke institisjoner',
+    'Thor Heyerdahl',
+    'Holmestrand',
+    'Fagskolen Vesfold og Telemark' */
+]
+const except = [
+]
+module.exports = { wordBook, except }
+
+// TODO - må nok slutte med ordbok - for mange navn som kødder til...
