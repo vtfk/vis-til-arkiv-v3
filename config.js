9f562e215116618c31e11f6e56cc079bedc0d3d1
jorgtho <jorgen.thorsnes@gmail.com>
2022-03-30 14:46:26 +0200

    config variables for ocr

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-  unavailable: true, // if the service should send email to user that it is unavailable, instead of archiving
+  unavailable: false, // if the service should send email to user that it is unavailable, instead of archiving
+  useOcr: true, // if you should run ocr on documents that cannot be recognized (requires set up of ocr)
+  ocrEngine: process.env.OCR_ENGINE || 'path to ocrMyPdf nodejs script',
   email: {
     fileNameEmailSeparator: process.env.FILE_NAME_EMAIL_SEPARATOR || '---',
     smtpHost: process.env.SMTP_HOST || 'smtp.mail.com',
@@ -12,8 +14,11 @@ module.exports = {
   },
   rootDirectory: process.env.ROOT_DIRECTORY || 'C:/VisTilArkiv',
   dispatchDirectoryName: process.env.DISPATCH_DIRECTORY_NAME || 'input',
+  ocrInputDirectory: process.env.OCR_INPUT_DIRECTORY_NAME || 'where ocr should take place',
+  ocrDispatchDirectoryName: process.env.OCR_DISPATCH_DIRECTORY_NAME || 'ocrInput',
   documentDirectoryName: process.env.DOCUMENT_DIRECTORY_NAME || 'documents',
   deleteDirectoryName: process.env.DELETE_DIRECTORY_NAME || 'delete',
+  originalsDirectoryName: process.env.ORIGINALS_DIRECTORY_NAME || 'originals',
   typeSearchWord: process.env.TYPE_SEARCH_WORD || 'VIS MAL TYPE',
   statFile: process.env.STAT_FILE || 'C:/VisTilArkiv/stat.json',
   deleteFinishedJobs: process.env.DELETE_FINISHED_JOBS || false,
