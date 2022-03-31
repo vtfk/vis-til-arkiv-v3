437727a8c9399908c4f00d16075ddd8f4c9effbf
jorgtho <jorgen.thorsnes@gmail.com>
2022-03-31 14:58:00 +0200

    better replacing of schools

--- data/wordBook.js
+++ data/wordBook.js
@@ -8,31 +8,35 @@ const wordBook = [
   'Dato:',
   'Sted:',
   'Skole:'
-  /*
-    'Bamble',
-    'Nøtterøy',
-    'Horten',
-    'Rjukan',
-    'Greveskogen',
-    'Skien',
-    'Kragerø',
-    'Notodden',
-    'Porsgrunn',
-    'Vest-Telemark',
-    'Hjalmar Johansen',
-    'Skogmo',
-    'Færder',
-    'Sande',
-    'Sandefjord',
-    'Kompetansebyggeren',
-    'Melsom',
-    'Skolen for sosiale og medisinke institisjoner',
-    'Thor Heyerdahl',
-    'Holmestrand',
-    'Fagskolen Vesfold og Telemark' */
+]
+const schools = [
+  'Bamble videregående skole',
+  'Nøtterøy videregående skole',
+  'Horten videregående skole',
+  'Rjukan videregående skole',
+  'Greveskogen videregående skole',
+  'Skien videregående skole',
+  'Kragerø videregående skole',
+  'Notodden videregående skole',
+  'Porsgrunn videregående skole',
+  'Vest-Telemark vidaregåande skule',
+  'Hjalmar Johansen videregående skole',
+  'Skogmo videregående skole',
+  'Færder videregående skole',
+  'Sande videregående skole',
+  'Sandefjord videregående skole',
+  'Re videregående skole',
+  'Bø vidaregåande skule',
+  'Kompetansebyggeren',
+  'Melsom videregående skole',
+  'Skolen for sosiale og medisinke institisjoner',
+  'Thor Heyerdahl videregående skole',
+  'Holmestrand videregående skole',
+  'Nome videregående skole',
+  'Fagskolen Vesfold og Telemark'
 ]
 const except = [
 ]
-module.exports = { wordBook, except }
+module.exports = { wordBook, except, schools }
 
 // TODO - må nok slutte med ordbok - for mange navn som kødder til...
